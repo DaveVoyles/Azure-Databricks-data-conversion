82f9b95c793a3fef2d9b4310ed65af52f725abca
Dave Voyles <Dnvoyles@gmail.com>
2020-04-03 16:39:06 -0400

    Added more filtering for dates

--- notebooks/Users/dvoyles@microsoft.com/csv+to+parquet.py
+++ notebooks/Users/dvoyles@microsoft.com/csv+to+parquet.py
@@ -117,5 +117,57 @@ writeParquetToStorage('csv')
 
 # COMMAND ----------
 
-#Read back the parquet file we just converted from csv -> parquet & stored in blob
+##Read back the parquet file we just converted from csv -> parquet & stored in blob
 parquetFile = readIncrementalParquetFile('02-04-2020_18-53')
+
+# COMMAND ----------
+
+parquetFile.show(30)
+
+# COMMAND ----------
+
+parquetFile.describe().show()
+
+# COMMAND ----------
+
+# How many distinct registration dates are there? If more than 1, Filter by that.
+parquetFile.select("registration_dttm").distinct().count()
+
+# COMMAND ----------
+
+parquetFile.filter(parquetFile.registration_dttm =="2016-02-03").count()
+
+# COMMAND ----------
+
+# Converts a Column of pyspark.sql.types.StringType or pyspark.sql.types.TimestampType into pyspark.sql.types.DateType 
+from pyspark.sql.functions import to_timestamp
+
+    #    df.select(to_timestamp(df.t                                                ).alias('dt')).collect()
+    #    df.select(to_timestamp(df.t,                          'yyyy-MM-dd HH:mm:ss').alias('dt')).collect()to
+to_timestamp =  parquetFile.select(to_timestamp(parquetFile.registration_dttm, 'yyyy-MM-dd HH:mm:ss').alias('dt')).collect()
+to_timestamp
+
+# COMMAND ----------
+
+# Filtered dates
+after_start_date  = parquetFile["registration_dttm"] >= start_date
+before_end_date   = parquetFile["registration_dttm"] <= end_date
+between_two_dates = after_start_date & before_end_date
+
+import datetime, time 
+from pyspark.sql import SQLContext
+
+# COMMAND ----------
+
+# parquetFile.filter(parquetFile.registration_dttm.between( ("2016-02-03"))
+import pyspark.sql.functions as func
+import datetime, time 
+from pyspark.sql import SQLContext
+
+#todate = parquetFile.select(func.to_date(parquetFile.registration_dttm).alias("time"))
+#sf = todate.filter(todate.time > date_from).filter(todate.time < date_to)
+
+dates = ("2016-02-03",  "2016-02-04")
+date_from, date_to = [to_date(lit(s)).cast(TimestampType()) for s in dates]
+
+parquetFile.where((sf.my_col > date_from) & (sf.my_col < date_to))
