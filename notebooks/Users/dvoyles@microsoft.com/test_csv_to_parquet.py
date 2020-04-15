e8c12817c51df0207af605d5244c3ba309a22415
Dave Voyles <dvoyles@microsoft.com>
2020-04-15 14:47:48 -0400

    create setup/teardown methods for local pyspark env

--- notebooks/Users/dvoyles@microsoft.com/test_csv_to_parquet.py
+++ notebooks/Users/dvoyles@microsoft.com/test_csv_to_parquet.py
@@ -1,30 +1,49 @@
 import unittest
 import pandas as pd
-# from csv_to_parquet import readAndShowCSV
+import logging
+from csv_to_parquet import readAndShowCSV
 from io             import BytesIO
 from unittest.mock  import patch
+from pyspark.sql import SparkSession
 
-# Inherits from unittest.TestCase
-# Gives us access to testing capibilities 
+# Inherits from unittest.TestCase, gives us access to testing capibilities 
 class TestParquet(unittest.TestCase):
+ 
+     ######## CREATE LOCAL PYSPARK ENV FOR TESTING ########
+    @classmethod
+    def suppress_py4j_logging(cls):
+        logger = logging.getLogger('py4j')
+        logger.setLevel(logging.WARN)
 
-    # def setUpClass(cls):
-    #     conf = pyspark.SparkConf().setMaster("local[*]").setAppName("testing")
-    #     cls.sc = pyspark.SparkContext(conf=conf)
-    #     cls.spark = pyspark.SQLContext(cls.sc)
+    @classmethod
+    def create_testing_pyspark_session(cls):
+        return (SparkSession.builder
+        .master('local[2]')
+        .appName('my-local-testing-pyspark-context')
+        .enableHiveSupport()
+        .getOrCreate())
 
-    # def tearDownClass(cls):
-    #     cls.sc.stop()
+    ######## SETUP AND TEAR DOWN ########
+    @classmethod
+    def setUpClass(cls):
+        cls.suppress_py4j_logging()
+        cls.spark = cls.create_testing_pyspark_session()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
+
+     ######## TESTED METHODS ########
     def test_readAndShowCSV(self):
         # csvFile = readAndShowCSV()
+           # test data file path, the fils is a csv file.
+        test_data_file_path = '../../../data/userdata1.csv'
 
         # Read CSV 
-        myCSV = pd.read_csv('../../../data/userdata1.csv')
+        myCSV = pd.read_csv(test_data_file_path)
         print(myCSV)
 
         # Compare first column of csv to our hard-coded column & assert equal
-
 
 # Will run all of our tests
 if __name__ == '__main__':
