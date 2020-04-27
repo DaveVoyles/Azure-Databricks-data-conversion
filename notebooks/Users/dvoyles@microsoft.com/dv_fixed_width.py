f77e061001ec3fd73e92aa8b0f59a320a856ed9d
Dave Voyles <Dnvoyles@gmail.com>
2020-04-27 13:59:23 -0400

    Read text file & write to blob as parquet

--- notebooks/Users/dvoyles@microsoft.com/dv_fixed_width.py
+++ notebooks/Users/dvoyles@microsoft.com/dv_fixed_width.py
@@ -25,26 +25,24 @@ dbutils.widgets.get("pipelineRunId")
 
 # COMMAND ----------
 
-#Supply storageName and accessKey values
+# Supply storageName and accessKey values
+# You can also use Azure Key Vault to abstract this by connecting Key Vault -> Databricks
 storageName = "dvhdinsighthdistorage"
-accessKey = "tb7o3VJklVaQ56nw6uqvZAFGfpx89QuXO7JeYntHoN3Mf5Tp7x7k30rHr00SiSGeNkhkr80bvRHdfzUqttzfTQ=="
-
-# Without this check, if directory is already mounted it will throw an error here.
-#if mount_point in [mp.mountPoint for mp in dbutils.fs.mounts()]:
-#    print(mount_pointt + " exists!")
-#else:
+accessKey    = "tb7o3VJklVaQ56nw6uqvZAFGfpx89QuXO7JeYntHoN3Mf5Tp7x7k30rHr00SiSGeNkhkr80bvRHdfzUqttzfTQ=="
 
 # Unmount blob storage if it currently exists. 
+# Without this, Databricks will throw an error each time you try to load data from storage
 def sub_unmount(str_path):
     if any(mount.mountPoint == str_path for mount in dbutils.fs.mounts()):
         dbutils.fs.unmount(str_path)
 
 sub_unmount('/mnt/adfdata')
 
+# Mount blob storage to Databricks
 try:
   dbutils.fs.mount(
-    source = "wasbs://sinkdata@"+storageName+".blob.core.windows.net/",
-    mount_point = "/mnt/adfdata",
+    source        = "wasbs://sinkdata@"+storageName+".blob.core.windows.net/",
+    mount_point   = "/mnt/adfdata",
     extra_configs = {"fs.azure.account.key."+storageName+".blob.core.windows.net":
                      accessKey})
 except Exception as e:
@@ -74,26 +72,12 @@ display(initialDF)
 
 # COMMAND ----------
 
-# Read fixed-width: https://stackoverflow.com/questions/41944689/pyspark-parse-fixed-width-text-file
-
-"""
-df = spark.read.text("/tmp/sample.txt")
-df.select(
-    df.value.substr(1,3).alias('id'),
-    df.value.substr(4,8).alias('date'),
-    df.value.substr(12,3).alias('string'),
-    df.value.substr(15,4).cast('integer').alias('integer')
-).show()
-
-
-+---+--------+------+-------+
-| id|    date|string|integer|
-+---+--------+------+-------+
-|001|01292017|   you|   1234|
-|002|01302017|    me|   5678|
-+---+--------+------+-------+
-+++
+#Removing Extension from filename
+import os
+file = os.path.splitext(getArgument("filename"))[0]
+print(file)
 
 # COMMAND ----------
 
-## May have to store data in here: https://www.element61.be/en/resource/how-can-we-use-azure-databricks-and-azure-data-factory-train-our-ml-algorithms
+# write the output into parquet
+initialDF.write.mode("overwrite").parquet("dbfs:/mnt/adfdata"+getArgument("output")+"/"+file+"_"+getArgument("pipelineRunId")+"/parquet") #for parquet
